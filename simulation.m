4869d0e806e1e84a2516315656d555b0e711ffa6
viktor-ktorvi <69254199+viktor-ktorvi@users.noreply.github.com>
2020-08-16 16:28:10 +0000

    Simulacija
    Automatizovano ranovanje, belezenje parametara u Excel tabeli u novom folderu i cuvanje figura poredjenja. Svaki ran se cuva u svom folderu u data/.

--- simulation.m
+++ simulation.m
@@ -5,29 +5,58 @@ clear variables;
 sim_file_name = 'mppt';
 open_system(sim_file_name);
 
-sim_duration = 0.5;
-transport_delay = 0;
-
 %% Parameters 
-    parameters.duty_min=0.28; % za oko 0.27 duty je Vin vece od Voc za 6V Vout, jer Vout = D*Vin;
-    parameters.duty_max=1.0;
-    parameters.duty_init=0.5;
-    
-    parameters.Voc = 21.9;
-    parameters.Isc = 1.84;
-    
-    parameters.N = 15;     % broj tacaka za diskretizsaciju stanja
+parameters.sim_duration = 0.03;
+parameters.transport_delay = 0;
 
-    parameters.min_step = 0.0025;    % dsp limit
-    parameters.big_step = 3*parameters.min_step;
+parameters.duty_min=0.28; % za oko 0.27 duty je Vin vece od Voc za 6V Vout, jer Vout = D*Vin;
+parameters.duty_max=1.0;
+parameters.duty_init=0.5;
 
-    parameters.wp = 1;    % positive reward coef
-    parameters.wn = 4;    % negative reward coef
-    
-    parameters.alpha = 0.2;    % learning rate
-    parameters.gamma = 0.9;    % discount facor
+parameters.Voc = 21.9;
+parameters.Isc = 1.84;
 
-    parameters.actions = [ -parameters.min_step, 0, parameters.min_step];    % actions
+parameters.N = 15;     % number of points for discretization of I and V
+
+parameters.min_step = 0.0025;    % dsp limit
+parameters.big_step = 3*parameters.min_step;
+
+parameters.wp = 1;    % positive reward coef
+parameters.wn = 4;    % negative reward coef
+
+parameters.alpha = 0.2;    % learning rate
+parameters.gamma = 0.9;    % discount facor
+
+parameters.actions = [ -parameters.min_step, 0, parameters.min_step];    % actions
 
 %% Simulation
-sim(sim_file_name)
+simout = sim(sim_file_name);
+
+%% Create folder
+initial_path = "data/";
+folder_name = "sim at ";
+folder_path = create_folder_at_time(initial_path, folder_name);
+
+%% Save figures
+time_series = simout.simout;
+
+fig = figure();
+plot(time_series)
+
+figure_name = "my_figure";
+figure_extension = ".jpg";
+saveas(fig,folder_path + figure_name + figure_extension);
+
+%% Excel
+
+excel_file_name = "parameters table";
+excel_extension = ".xlsx";
+writetable(struct2table(parameters), folder_path + excel_file_name + excel_extension);
+
+complete_path = "C:\Users\HP\Desktop\Faks\Matlab\Energetika\MPPT_avgust2020\";
+modify_excel_columns(complete_path + folder_path + excel_file_name + excel_extension);
+
+%% Save signals
+time_series_name = "output";
+time_series_extension = ".mat";
+save(folder_path + time_series_name + time_series_extension,'time_series')
