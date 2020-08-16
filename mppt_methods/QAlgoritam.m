f06bfac388ad57a0b25b46d3a4e4ee5f1c6c7fd5
viktor-ktorvi <69254199+viktor-ktorvi@users.noreply.github.com>
2020-08-16 11:11:06 +0000

    detektovanje ss i resetovanje Q table
    Dakle broji uzastopne 0 akcije i ako ih je mnogo (>200 msm) onda proglasava steady state
    Kada izadjemo iz steady state-a resetuje Q matricu

--- mppt_methods/QAlgoritam.m
+++ mppt_methods/QAlgoritam.m
@@ -1,4 +1,4 @@
-function [output] = QAlgoritam(V, I, epsilon, parameters)
+function [output, steady_state_flag] = QAlgoritam(V, I, epsilon, parameters)
 %% Constants
     duty_min = parameters.duty_min;
     duty_max = parameters.duty_max;
@@ -18,7 +18,7 @@ function [output] = QAlgoritam(V, I, epsilon, parameters)
     
 %% Initializing
 
-    persistent Vold Pold Iold duty_old Q prev_action_index prev_current_index prev_voltage_index prev_deg_index;
+    persistent Vold Pold Iold duty_old Q prev_action_index prev_current_index prev_voltage_index prev_deg_index consecutive_zero_actions prev_steady_state_flag;
     
     if isempty(Vold)
         Vold=0;
@@ -33,6 +33,9 @@ function [output] = QAlgoritam(V, I, epsilon, parameters)
 
         % N x I, N x V , 2 x Deg, length(actions) x actions
         Q = zeros(N, N, 2, length(actions)); 
+        
+        consecutive_zero_actions = 0;
+        prev_steady_state_flag = 0;
         
     end  
     
@@ -67,9 +70,6 @@ function [output] = QAlgoritam(V, I, epsilon, parameters)
     % update
     Q(prev_current_index, prev_voltage_index, prev_deg_index, prev_action_index) = (1 - alpha) .* Q(prev_current_index, prev_voltage_index, prev_deg_index, prev_action_index) + alpha .* (reward + gamma * max(Q(current_index, voltage_index, Deg_index, :)));
     
-    % debug Q table
-    Qq = Q;
-    
     % best move
     max_val = max(Q(current_index, voltage_index, Deg_index, :));
     
@@ -88,10 +88,40 @@ function [output] = QAlgoritam(V, I, epsilon, parameters)
         prev_action_index = random_index;
     end
     
+%% Checking for steady state
+    if consecutive_zero_actions == 0
+        if actions(prev_action_index) == 0
+            consecutive_zero_actions = 1;
+        end
+    else
+        if actions(prev_action_index) == 0
+            consecutive_zero_actions = consecutive_zero_actions + 1;
+        else
+            consecutive_zero_actions = 0;
+        end
+    end
+    
+    cza_threshold = 200;
+    if consecutive_zero_actions > cza_threshold
+        steady_state_flag = 1;
+    else
+        steady_state_flag = 0;
+    end
+    
+    % if something changed reset everything
+    if prev_steady_state_flag == 1 && steady_state_flag == 0
+
+        % N x I, N x V , 2 x Deg, length(actions) x actions
+        Q = zeros(N, N, 2, length(actions)); 
+        prev_action_index =  floor(length(actions)/2) + 1;   % 0 action
+        consecutive_zero_actions = 0;
+    end
+    
 %% Clamping 
     output = clamp(output, duty_min, duty_max);
     
 %% Saving old values
+    prev_steady_state_flag = steady_state_flag;
     prev_current_index = current_index;
     prev_voltage_index = voltage_index;
     prev_deg_index = Deg_index;
