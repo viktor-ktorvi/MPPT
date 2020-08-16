1a0c713984ed0ee4225e7edd8da79991d7ec5452
viktor-ktorvi <69254199+viktor-ktorvi@users.noreply.github.com>
2020-08-16 11:06:39 +0000

    Simulacija
    Ako nije bilo ranije sada prosledjuje transport_delay (0 za sada) i sim_duration

--- simulation.m
+++ simulation.m
@@ -5,6 +5,9 @@ clear variables;
 sim_file_name = 'mppt';
 open_system(sim_file_name);
 
+sim_duration = 0.5;
+transport_delay = 0;
+
 %% Parameters 
     parameters.duty_min=0.28; % za oko 0.27 duty je Vin vece od Voc za 6V Vout, jer Vout = D*Vin;
     parameters.duty_max=1.0;
@@ -13,7 +16,7 @@ open_system(sim_file_name);
     parameters.Voc = 21.9;
     parameters.Isc = 1.84;
     
-    parameters.N = 10;     % broj tacaka za diskretizsaciju stanja
+    parameters.N = 15;     % broj tacaka za diskretizsaciju stanja
 
     parameters.min_step = 0.0025;    % dsp limit
     parameters.big_step = 3*parameters.min_step;
@@ -21,10 +24,10 @@ open_system(sim_file_name);
     parameters.wp = 1;    % positive reward coef
     parameters.wn = 4;    % negative reward coef
     
-    parameters.alpha = 0.5;    % learning rate
+    parameters.alpha = 0.2;    % learning rate
     parameters.gamma = 0.9;    % discount facor
 
-    parameters.actions = [ -parameters.big_step, 0, parameters.big_step];    % actions
+    parameters.actions = [ -parameters.min_step, 0, parameters.min_step];    % actions
 
 %% Simulation
 sim(sim_file_name)
